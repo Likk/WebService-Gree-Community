e45952e02f5bf74025072bf38003abb6e6a15306
likkradyus <git@li.que.jp>
2014-04-05 19:19:02 +0900

    2space indent -> 4space indent.

--- lib/WebService/Gree/Community.pm
+++ lib/WebService/Gree/Community.pm
@@ -50,18 +50,18 @@ WebService::AipoLiveオブジェクトの作成
 =cut
 
 sub new {
-  my $class = shift;
-  my %args  = @_;
-  $args{agent}      ||= __PACKAGE__." ".$VERSION;
-  $args{mech}         = WWW::Mechanize->new(
-                            agent => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0',
-                          );
-  $args{root}         = 'http://t.gree.jp';
-  $args{last_req}  = time;
-  $args{interval}  = 3; #sec.
-  my $self = bless {%args}, $class;
-  $self->login;
-  return $self;
+    my $class = shift;
+    my %args  = @_;
+    $args{agent}      ||= __PACKAGE__." ".$VERSION;
+    $args{mech}         = WWW::Mechanize->new(
+        agent => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0',
+    );
+    $args{root}         = 'http://t.gree.jp';
+    $args{last_req}  = time;
+    $args{interval}  = 3; #sec.
+    my $self = bless {%args}, $class;
+    $self->login;
+    return $self;
 }
 
 =head1 METHOD
@@ -73,15 +73,15 @@ sub new {
 =cut
 
 sub login {
-  my $self = shift;
-  my $post = {
-    action        => 'id_login',
-    backto        => '',
-    user_mail     => $self->{mail_address},
-    user_password => $self->{password},
-    login_status  => 1,
-  };
-  my $res = $self->post($self->{root}, $post);
+    my $self = shift;
+    my $post = {
+        action        => 'id_login',
+        backto        => '',
+        user_mail     => $self->{mail_address},
+        user_password => $self->{password},
+        login_status  => 1,
+    };
+    my $res = $self->post($self->{root}, $post);
 }
 
 =head2 post
@@ -91,9 +91,9 @@ mech post with interval.
 =cut
 
 sub post {
-  my $self = shift;
-  $self->_sleep_interval;
-  $self->{'mech'}->post(@_);
+    my $self = shift;
+    $self->_sleep_interval;
+    $self->{'mech'}->post(@_);
 }
 
 =head2 get
@@ -103,9 +103,9 @@ mech get with interval.
 =cut
 
 sub get {
-  my $self = shift;
-  $self->_sleep_interval;
-  $self->{'mech'}->get(@_);
+    my $self = shift;
+    $self->_sleep_interval;
+    $self->{'mech'}->get(@_);
 }
 
 =head2 get_members
@@ -115,16 +115,16 @@ sub get {
 =cut
 
 sub get_members {
-  my $self = shift;
-  my %args = @_;
-  my $community_id = $args{community_id} ? $args{community_id} : $self->{community_id};
-  my $members = [];
-  my $member_count = $self->_perse_member_count($community_id);
-  for my $offset (0..$member_count/10){
-    my $page_member = $self->_parse_community_members($community_id, $offset * 10) || [];
-    push @$members, @$page_member;
-  }
-  return $members;
+    my $self = shift;
+    my %args = @_;
+    my $community_id = $args{community_id} ? $args{community_id} : $self->{community_id};
+    my $members = [];
+    my $member_count = $self->_perse_member_count($community_id);
+    for my $offset (0..$member_count/10){
+        my $page_member = $self->_parse_community_members($community_id, $offset * 10) || [];
+        push @$members, @$page_member;
+    }
+    return $members;
 }
 
 =head2 get_bbs_list
@@ -134,19 +134,19 @@ sub get_members {
 =cut
 
 sub get_bbs_list {
-  my $self = shift;
-  my %args = @_;
-  my $community_id = $args{community_id} ? $args{community_id} : $self->{community_id};
-  my $bbs_list = [];
-  my $offset   = 0;
-  while (1){
-    my $res = $self->get("@{[$self->{root}]}/?community_id=@{[$community_id]}&action=community_bbs_list&more=1&offset=@{[$offset]}");
-    my $bbs_list_wk = $self->_parse_bbs_list($res->decoded_content()) || [];
-    push @$bbs_list, @$bbs_list_wk;
-    last if scalar @$bbs_list_wk < 10;
-    $offset += 10;
-  }
-  return $bbs_list;
+    my $self = shift;
+    my %args = @_;
+    my $community_id = $args{community_id} ? $args{community_id} : $self->{community_id};
+    my $bbs_list = [];
+    my $offset   = 0;
+    while (1){
+        my $res = $self->get("@{[$self->{root}]}/?community_id=@{[$community_id]}&action=community_bbs_list&more=1&offset=@{[$offset]}");
+        my $bbs_list_wk = $self->_parse_bbs_list($res->decoded_content()) || [];
+        push @$bbs_list, @$bbs_list_wk;
+        last if scalar @$bbs_list_wk < 10;
+        $offset += 10;
+    }
+    return $bbs_list;
 }
 
 =head2 show_bbs
@@ -156,14 +156,14 @@ bbs内の閲覧
 =cut
 
 sub show_bbs {
-  my $self = shift;
-  my %args = @_;
-  my $page = $self->{page} || 1;
-  my $community_id = $args{community_id} ? $args{community_id} : $self->{community_id};
-  my $thread_id    = $args{thread_id};
-  my $offset       = ($page - 1) * 5;
-  my $res = $self->get("@{[$self->{root}]}/?community_id=@{[$community_id]}&thread_id=@{[$thread_id]}&action=community_bbs_view&offset=@{[$offset]}");
-  $self->_parse_bbs($res->decoded_content);
+    my $self = shift;
+    my %args = @_;
+    my $page = $self->{page} || 1;
+    my $community_id = $args{community_id} ? $args{community_id} : $self->{community_id};
+    my $thread_id    = $args{thread_id};
+    my $offset       = ($page - 1) * 5;
+    my $res = $self->get("@{[$self->{root}]}/?community_id=@{[$community_id]}&thread_id=@{[$thread_id]}&action=community_bbs_view&offset=@{[$offset]}");
+    $self->_parse_bbs($res->decoded_content);
 }
 
 =head1 PRIVATE METHODS.
@@ -177,10 +177,10 @@ sub show_bbs {
 =cut
 
 sub _sleep_interval {
-  my $self = shift;
-  my $wait = $self->{interval} - (time - $self->{last_req});
-  sleep $wait if $wait > 0;
-  $self->{last_req} = time;
+    my $self = shift;
+    my $wait = $self->{interval} - (time - $self->{last_req});
+    sleep $wait if $wait > 0;
+    $self->{last_req} = time;
 }
 
 =item B<_perse_member_count>
@@ -190,21 +190,21 @@ sub _sleep_interval {
 =cut
 
 sub _perse_member_count {
-  my $self = shift;
-  my $community_id = shift;
-  my $res     = $self->get("@{[$self->{root}]}/?action=community_bbs_list&community_id=@{[$community_id]}&from_tsns=stream_community&group=community");
-  my $content = $res->decoded_content();
+    my $self = shift;
+    my $community_id = shift;
+    my $res     = $self->get("@{[$self->{root}]}/?action=community_bbs_list&community_id=@{[$community_id]}&from_tsns=stream_community&group=community");
+    my $content = $res->decoded_content();
 
-  my $scraper = scraper {
-    process '//div[@class="txt"]/table[1]/tbody/tr[2]/td', member => 'TEXT';
-    result 'member';
-  };
-  my $result = $scraper->scrape($content);
+    my $scraper = scraper {
+        process '//div[@class="txt"]/table[1]/tbody/tr[2]/td', member => 'TEXT';
+        result 'member';
+    };
+    my $result = $scraper->scrape($content);
 
-  #1,000人 の数値のところだけをとって、カンマを取り除く
-  $result =~ s/^((\d|,)+)?.*$/$1/;
-  $result =~ s/,//g;
-  return $result;
+    #1,000人 の数値のところだけをとって、カンマを取り除く
+    $result =~ s/^((\d|,)+)?.*$/$1/;
+    $result =~ s/,//g;
+    return $result;
 }
 
 =item B<_parse_community_members>
@@ -214,22 +214,22 @@ sub _perse_member_count {
 =cut
 
 sub _parse_community_members {
-  my $self   = shift;
-  my $community_id = shift;
-  my $offset = shift;
-  my $members = [];
-  my $res     = $self->get("@{[$self->{root}]}/?action=community_view_joinlist&community_id=@{[$community_id]}&group=community&offset=@{[$offset]}&tab=community_members&more=1");
-  my $content = $res->decoded_content();
-  my $scraper = scraper {
-    process '//div[@class="followerList clearfix community_view_joinlist"]', 'members[]' => '@id';
-    result 'members';
-  };
-  my $result = $scraper->scrape($content);
-  for my $div_id (@$result){
-    $div_id =~ s/community_view_joinlist-//g;
-    push @$members, $div_id;
-  }
-  return $members;
+    my $self   = shift;
+    my $community_id = shift;
+    my $offset = shift;
+    my $members = [];
+    my $res     = $self->get("@{[$self->{root}]}/?action=community_view_joinlist&community_id=@{[$community_id]}&group=community&offset=@{[$offset]}&tab=community_members&more=1");
+    my $content = $res->decoded_content();
+    my $scraper = scraper {
+        process '//div[@class="followerList clearfix community_view_joinlist"]', 'members[]' => '@id';
+        result 'members';
+    };
+    my $result = $scraper->scrape($content);
+    for my $div_id (@$result){
+        $div_id =~ s/community_view_joinlist-//g;
+        push @$members, $div_id;
+    }
+    return $members;
 }
 
 =item B<_bbs_list>
@@ -239,40 +239,40 @@ sub _parse_community_members {
 =cut
 
 sub _parse_bbs_list {
-  my $self = shift;
-  my $html = shift;
-  my $bbs_list = [];
+    my $self = shift;
+    my $html = shift;
+    my $bbs_list = [];
 
-  my $scraper = scraper {
-    process '//a', 'data[]' => scraper {
-      process '//div[@class="txt"]/div[1]/span[@class="userName"]', title       => 'TEXT';
-      process '//div[@class="txt"]/div[1]/span[@class="count"]',    count       => 'TEXT';
-      process '//div[@class="txt"]//div[@class="description"]',     description => 'TEXT';
-      process '//div[@class="txt"]//div[@class="timestamp"]',       timestamp   => 'TEXT';
+    my $scraper = scraper {
+        process '//a', 'data[]' => scraper {
+            process '//div[@class="txt"]/div[1]/span[@class="userName"]', title       => 'TEXT';
+            process '//div[@class="txt"]/div[1]/span[@class="count"]',    count       => 'TEXT';
+            process '//div[@class="txt"]//div[@class="description"]',     description => 'TEXT';
+            process '//div[@class="txt"]//div[@class="timestamp"]',       timestamp   => 'TEXT';
+        };
+        process '//a', 'script[]' => '@onclick';
     };
-    process '//a', 'script[]' => '@onclick';
-  };
-  my $bbs_list_wk = $scraper->scrape($html);
-  for my $index (0.. $#{$bbs_list_wk->{data}}){
+    my $bbs_list_wk = $scraper->scrape($html);
+    for my $index (0.. $#{$bbs_list_wk->{data}}){
 
-    #thread_id が onclick に入り込んでるのでそれを取り出してやる
-    my $script = $bbs_list_wk->{script}->[$index];
-    $script =~ s{^(?:.*)?'thread_id':'(\d+)'(.*)?$}{$1};
+        #thread_id が onclick に入り込んでるのでそれを取り出してやる
+        my $script = $bbs_list_wk->{script}->[$index];
+        $script =~ s{^(?:.*)?'thread_id':'(\d+)'(.*)?$}{$1};
 
 
-    #valueの前後に半角空白が入るので trim してやる
-    my $data = $bbs_list_wk->{data}->[$index];
-    while(my($k, $v) = each %$data){
-      $data->{$k} = trim($v);
+        #valueの前後に半角空白が入るので trim してやる
+        my $data = $bbs_list_wk->{data}->[$index];
+        while(my($k, $v) = each %$data){
+            $data->{$k} = trim($v);
+        }
+
+        #整形して結果にpush
+        push @$bbs_list, {
+            %{$data},
+            link => $script,
+        };
     }
-
-    #整形して結果にpush
-    push @$bbs_list, {
-      %{$data},
-      link => $script,
-    };
-  }
-  return $bbs_list;
+    return $bbs_list;
 }
 
 =item B<_parse_bbs>
@@ -282,40 +282,40 @@ sub _parse_bbs_list {
 =cut
 
 sub _parse_bbs {
-  my $self = shift;
-  my $html = shift;
-  my $result = [];
-  my $scraper = scraper {
-    process '//div[@class="comment comment-text community_bbs_view"]', 'divs[]' => scraper {
-      process '//div[@class="userName"]/div[@class="nickname"]', user_name => 'TEXT';
-      process '//div[@class="comTxt"]',                          description => 'TEXT';
-      process '//div[@class="timestamp"]',                       timestamp => 'TEXT';
-    }, 'ids[]' => '@id';
+    my $self = shift;
+    my $html = shift;
+    my $result = [];
+    my $scraper = scraper {
+        process '//div[@class="comment comment-text community_bbs_view"]', 'divs[]' => scraper {
+            process '//div[@class="userName"]/div[@class="nickname"]', user_name => 'TEXT';
+            process '//div[@class="comTxt"]',                          description => 'TEXT';
+            process '//div[@class="timestamp"]',                       timestamp => 'TEXT';
+        }, 'ids[]' => '@id';
 
-  };
-
-  my $data = $scraper->scrape($html);
-  my $index = 0;
-  #comment_idを取り出す
-  my $ids =  [ grep { $_ =~ s{^msg-(\d+)$}{$1} } @{$data->{ids}} ];
-
-  for my $div (@{$data->{divs}}){
-    #関係ない div を除去する
-
-    #valueの前後に半角空白が入るので trim してやる
-    while(my($k, $v) = each %$div){
-      $div->{$k} = trim($v);
-    }
-
-    #整形 & comment_id加えつつ結果にpush
-    push @$result, {
-      %$div,
-      id => $ids->[$index],
     };
-    $index++;
-  }
-  $result = [sort {$a->{id} <=> $b->{id} } @$result ];
-  return $result;
+
+    my $data = $scraper->scrape($html);
+    my $index = 0;
+    #comment_idを取り出す
+    my $ids =  [ grep { $_ =~ s{^msg-(\d+)$}{$1} } @{$data->{ids}} ];
+
+    for my $div (@{$data->{divs}}){
+        #関係ない div を除去する
+
+        #valueの前後に半角空白が入るので trim してやる
+        while(my($k, $v) = each %$div){
+            $div->{$k} = trim($v);
+        }
+
+        #整形 & comment_id加えつつ結果にpush
+        push @$result, {
+            %$div,
+            id => $ids->[$index],
+        };
+        $index++;
+    }
+    $result = [sort {$a->{id} <=> $b->{id} } @$result ];
+    return $result;
 }
 
 1;
